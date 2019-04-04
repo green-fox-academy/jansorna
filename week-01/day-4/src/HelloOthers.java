d2c790cbbd9f3ec1416c0fb793c24275210c1b76
Jan Sorna <honzasorna@gmail.com>
2019-04-04 13:58:04 +0200

    Completed HelloWorld exercises and uploaded .gitignore by unecessary IDEA J files

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Lucia!");
+        System.out.println("Hello, Matus!");
+        System.out.println("Hello, Juraj!");
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+    }
+}
