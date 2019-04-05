eb5606da35a026889df172e0987d04dd78cfe0d6
Jan Sorna <honzasorna@gmail.com>
2019-04-05 11:56:59 +0200

    Finished first 3 exercises from loops

---
+++ week-01/day-5/src/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+
+        int a;
+
+        for (a = 0; a < 100; a++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+
+    }
+}
---
+++ week-01/day-5/src/PrintEven.java
@@ -0,0 +1,11 @@
+public class PrintEven {
+    public static void main (String[] args) {
+
+        int a;
+        for (a = 2; a <= 500; a +=2) {
+            System.out.println(a);
+        }
+
+        // Create a program that prints all the even numbers between 0 and 500
+    }
+}
