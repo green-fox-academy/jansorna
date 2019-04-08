aa3107e43dca0374327aaaa017b7ca88d496543d
Jan Sorna <honzasorna@gmail.com>
2019-04-08 16:55:15 +0200

    Finished Append A in arrays, swapelement, Sumelements

---
+++ week-02/day-1/src/Swapelements.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Swapelements {
+
+    public static void main(String[] args) {
+        String[] abc = {"first", "second", "third"};
+
+        abc[0] = abc[2];
+        abc[2] = "first";
+
+        System.out.println(Arrays.toString(abc));
+    }
+
+    // - Create an array variable named `abc`
+    //   with the following content: `["first", "second", "third"]`
+    // - Swap the first and the third element of `abc`
+    }
+
+
