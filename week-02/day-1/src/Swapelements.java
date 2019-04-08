010e240297692c51bdea53519f82ffa7904352b3
Jan Sorna <honzasorna@gmail.com>
2019-04-08 17:12:10 +0200

    Improved Swapelement exercise

--- week-02/day-1/src/Swapelements.java
+++ week-02/day-1/src/Swapelements.java
@@ -5,8 +5,9 @@ public class Swapelements {
     public static void main(String[] args) {
         String[] abc = {"first", "second", "third"};
 
+        String temp = abc[0];
         abc[0] = abc[2];
-        abc[2] = "first";
+        abc[2] = temp;
 
         System.out.println(Arrays.toString(abc));
     }
