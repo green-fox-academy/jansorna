f67ad47aa2565cb554a6b780efd66c6c01fb3286
Jan Sorna <honzasorna@gmail.com>
2019-04-08 15:47:39 +0200

    Finished bloody Matrix and Double Items exercices

---
+++ week-02/day-1/src/Matrix.java
@@ -0,0 +1,35 @@
+public class Matrix {
+    public static void main(String[] args) {
+
+        int matrixDimension = 4;
+        int [][] matrix = new int[matrixDimension][matrixDimension];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == j) {
+                    matrix [i][j] = 1;
+                } else {
+                    matrix [i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix [i] [j]);
+            }
+            System.out.println();
+        }
+    }
+
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+}
+
