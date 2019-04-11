2940f5a4eca8369434b89b072ad8ecee072fb613
Jan Sorna <honzasorna@gmail.com>
2019-04-11 16:15:40 +0200

    Finished bloody Fibonacci exercise

--- week-02/day-4/src/Fibonacci.java
+++ week-02/day-4/src/Fibonacci.java
@@ -1,13 +1,27 @@
+import java.lang.reflect.Array;
+
 public class Fibonacci {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        int howMuchTimes = 5;
+        int howMuchTimes = 10;
 
+        for (int i = 0; i <= howMuchTimes - 1; i++) {
+
+            System.out.print(fibonacciRecursive(i) + ", ");
+
+        }
+
+    }
 
     public static int fibonacciRecursive (int n) {
 
-
+        if (n == 1 || n == 2) {
+            return 1;
+        } else if (n == 0){
+            return 0;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 
     }
 }
