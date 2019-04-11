e0e7ba43b4c312fbd3d9cbab5c13b7443df46e37
Jan Sorna <honzasorna@gmail.com>
2019-04-11 13:15:11 +0200

    Finished bloody String exercise

---
+++ week-02/day-4/src/Strings.java
@@ -0,0 +1,31 @@
+public class Strings {
+
+    public static void main (String[] args) {
+
+        String inputWord = "xxxxxxxXXXXxxxxxx";
+        int numberOfLetters = inputWord.length();
+        int counter = 0;
+
+        System.out.println(stringChanger(inputWord, numberOfLetters, counter));
+    }
+
+    public static String stringChanger(String word, int numberOfLetters, int number) {
+
+        char x = 'x';
+        char y = 'y';
+
+        if (number == numberOfLetters) {
+
+            return word;
+
+        } else if (word.charAt(number) == x){
+
+            return stringChanger((word.substring(0, number) + y + word.substring(number + 1)), numberOfLetters, number + 1);
+
+        } else {
+
+            return stringChanger(word, numberOfLetters, number + 1);
+
+        }
+    }
+}
