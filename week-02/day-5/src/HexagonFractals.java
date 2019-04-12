9e21b95d6babeebb1a0c1e56df12417667807d1f
Jan Sorna <honzasorna@gmail.com>
2019-04-12 18:49:42 +0200

    Finished Hexagon exercise

--- week-02/day-5/src/HexagonFractals.java
+++ week-02/day-5/src/HexagonFractals.java
@@ -7,31 +7,39 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class HexagonFractals {
     public static void mainDraw(Graphics graphics) {
 
-        int[] xPoints = {WIDTH / 4, WIDTH * 3 / 4, WIDTH, WIDTH * 3 / 4, WIDTH / 4, 0};
-        int[] yPoints = {0, 0, HEIGHT / 2, HEIGHT, HEIGHT, HEIGHT / 2};
+        int x0 = WIDTH / 4;
+        int x1 = WIDTH * 3 / 4;
+        int x2 = WIDTH;
+        int x3 = WIDTH * 3 / 4;
+        int x4 = WIDTH / 4;
+        int x5 = 0;
+
+        int y0 = 0;
+        int y1 = 0;
+        int y2 = HEIGHT / 2;
+        int y3 = HEIGHT;
+        int y4 = HEIGHT;
+        int y5 = HEIGHT / 2;
+
         int counter = 5;
 
-        drawFractalHexagon1(counter, xPoints, yPoints, WIDTH, HEIGHT, 6, graphics);
-        //drawFractalHexagon2(counter, xPoints, yPoints, WIDTH, HEIGHT, 6, graphics);
-        //drawFractalHexagon3(counter, xPoints, yPoints, WIDTH, HEIGHT, 6, graphics);
-
+        drawFractalHexagon(counter, x0, x1, x2, x3, x4, x5, y0, y1, y2, y3, y4, y5, WIDTH, HEIGHT, 6, graphics);
     }
 
-    public static void drawFractalHexagon1(int counter, int[] x, int[] y, int width, int height, int points, Graphics graphics) {
-
-        int[] m = {x[0],x[1] - width / 4,x[2] - width * 3 / 8, x[3] - width / 4,x[4],  x[5] + width / 8};
-        int[] n = {y[0], y[1], y[2] - height / 4, y[3] - height / 2, y[4] - height / 2, y[5] - height / 4};
+    public static void drawFractalHexagon(int counter, int x0, int x1, int x2, int x3, int x4, int x5, int y0, int y1, int y2, int y3, int y4, int y5, int width, int height, int points, Graphics graphics) {
 
         if (counter > 0) {
+            int[] x = {x0, x1, x2, x3, x4, x5};
+            int[] y = {y0, y1, y2, y3, y4, y5};
 
             graphics.drawPolygon(x, y, points);
 
-            drawFractalHexagon1(counter - 1, m, n, width / 2, height / 2, points, graphics);
-            drawFractalHexagon1(counter - 1, m, n, width / 2, height / 2, points, graphics);
+            drawFractalHexagon(counter - 1, x0, x1 - width / 4, x2 - width * 3 / 8, x3 - width / 4, x4, x5 + width / 8, y0, y1, y2 - height / 4, y3 - height / 2, y4 - height / 2, y5 - height / 4, height / 2, width / 2, points, graphics);
+            drawFractalHexagon(counter - 1, x0, x1 - width / 4, x2 - width * 3 / 8, x3 - width / 4, x4, x5 + width / 8, y0 + height / 2, y1 + height / 2, y2 + height / 4, y3, y4, y5 + height / 4, height / 2, width / 2, points, graphics);
+            drawFractalHexagon(counter - 1, x0 + width * 3 / 8, x1 + width / 8, x2, x3 + width / 8, x4 + width * 3 / 8, x5 + width / 2, y0 + height / 4, y1 + height / 4, y2, y3 - height / 4, y4 - height / 4, y5, height / 2, width / 2, points, graphics);
         }
     }
 
-    // Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 320;
 
