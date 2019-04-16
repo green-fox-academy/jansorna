023d11c9b808867df0381d0d5b468bbcd6558e9d
Jan Sorna <honzasorna@gmail.com>
2019-04-16 16:23:39 +0200

    Finished Petrol Station exercise

---
+++ week-03/day-2/src/PetrolStation/Car.java
@@ -0,0 +1,11 @@
+package PetrolStation;
+
+public class Car {
+    int gasAmount;
+    int capacity;
+
+    public Car() {
+        capacity = 100;
+        gasAmount = 0;
+    }
+}
---
+++ week-03/day-2/src/PetrolStation/Main.java
@@ -0,0 +1,18 @@
+package PetrolStation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //start testing
+        Station firstStation = new Station(5000);
+
+        Car porsche = new Car();
+
+        firstStation.refill(porsche);
+
+        System.out.println(firstStation.gasAmount);
+        System.out.println(porsche.capacity);
+        System.out.println(porsche.gasAmount);
+        //end testing
+    }
+}
