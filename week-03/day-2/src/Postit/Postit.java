6f60ac6720523f190a2044831fa24fe9866cc23f
Jan Sorna <honzasorna@gmail.com>
2019-04-16 12:29:09 +0200

    Finished Post it and Blog Post exercises

---
+++ week-03/day-2/src/Postit/Postit.java
@@ -0,0 +1,14 @@
+package Postit;
+
+public class Postit {
+
+    String backgroundColor;
+    String text;
+    String textColor;
+
+    public Postit(String backgroundColor, String text, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+}
---
+++ week-03/day-2/src/Postit/main.java
@@ -0,0 +1,11 @@
+package Postit;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Postit firstPost = new Postit("orange", "Idea 1", "blue");
+        Postit secondPost = new Postit("pink", "Awesome", "black");
+        Postit thirdPost = new Postit("yellow", "Superb!", "green");
+    }
+}
