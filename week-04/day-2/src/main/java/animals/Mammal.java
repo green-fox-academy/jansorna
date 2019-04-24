6f4bb5182b1d35e37d95b01be67c800125624ab8
Jan Sorna <honzasorna@gmail.com>
2019-04-24 15:20:24 +0200

    Finished Zoo exercise

---
+++ week-04/day-2/src/main/java/animals/Mammal.java
@@ -0,0 +1,20 @@
+package main.java.animals;
+
+public class Mammal extends Animal {
+
+    public Mammal(String name) {
+        super(name);
+    }
+
+
+    @Override
+    public String breed() {
+        return "pushing miniature versions out";
+    }
+
+    @Override
+    public String cry() {
+        return "Mamam-mamal";
+    }
+
+}
