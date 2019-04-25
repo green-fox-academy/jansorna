8e6803c1121521288771664d143aa317a6dc5822
Jan Sorna <honzasorna@gmail.com>
2019-04-25 14:34:54 +0200

    Improved test in Count Letters exercise

--- week-04/day-4/src/test/java/CountLettersTest.java
+++ week-04/day-4/src/test/java/CountLettersTest.java
@@ -1,22 +1,31 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class CountLettersTest {
     String firstString;
     CountLetters myCountLetters = new CountLetters(firstString);
     char[] lettersOfTheStringInArray;
+    HashMap<Character, Integer> exampleMap = new HashMap<>();
 
     @Before
-    public void beforeMethod(){
-        this.firstString = "Hello";
+    public void beforeMethod() {
+        this.firstString = "He";
         lettersOfTheStringInArray = firstString.toCharArray();
-
+        exampleMap.put('H', 1);
+        exampleMap.put('E', 1);
     }
 
     @Test
     public void getOccurence_inputOneWord_toReturnAndPrintOutDictionary() {
+        assertEquals(exampleMap, myCountLetters.getOccurence(firstString));
+    }
+
+    @Test
+    public void getOccurence_inputOneWord_MapIsNotEmpty() {
         assertNotNull(myCountLetters.getOccurence(firstString));
     }
 }
