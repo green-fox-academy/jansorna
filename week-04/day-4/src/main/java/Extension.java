0daa1598f072da343f85b92e8112ca69a7addc80
Jan Sorna <honzasorna@gmail.com>
2019-04-25 14:54:34 +0200

    Finished Extension exercise - don`t fully understand the instruction

--- week-04/day-4/src/main/java/Extension.java
+++ week-04/day-4/src/main/java/Extension.java
@@ -2,22 +2,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Extension {
-    int add(int a, int b) {
-        return 5;
+
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    int maxOfThree(int a, int b, int c) {
-        if (a > b)
-            return a;
-        else
-            return c;
+    public int maxOfThree(int a, int b, int c) {
+        int max = a;
+
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+        return max;
     }
 
-    int median(List<Integer> pool) {
-        return pool.get((pool.size()-1)/2);
+    public int median(List<Integer> pool) {
+        return pool.get((pool.size() - 1) / 2);
     }
 
-    boolean isVowel(char c) {
+    public boolean isVowel(char c) {
         return Arrays.asList('a', 'u', 'o', 'e', 'i').contains(c);
     }
 
@@ -27,9 +33,9 @@ public class Extension {
         for (int i = 0; i < length; i++) {
             char c = teve.charAt(i);
             if (isVowel(c)) {
-                teve = String.join(c + "v" + c, teve.split(""+c));
-                i+=2;
-                length+=2;
+                teve = String.join(c + "v" + c, teve.split("" + c));
+                i += 2;
+                length += 2;
             }
         }
         return teve;
--- week-04/day-4/src/test/java/ExtensionTest.java
+++ week-04/day-4/src/test/java/ExtensionTest.java
@@ -1,60 +1,62 @@
-import java.util.Arrays;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
 
-class ExtensionTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class ExtensionTest {
 
     Extension extension = new Extension();
 
     @Test
-    void testAdd_2and3is5() {
+   public void testAdd_2and3is5() {
         assertEquals(5, extension.add(2, 3));
     }
 
     @Test
-    void testAdd_1and4is5() {
-        assertEquals(5, extension.add(1, 4));
+    public void testAdd_2and4is5() {
+        assertEquals(6, extension.add(2, 4));
     }
 
     @Test
-    void testMaxOfThree_first() {
-        assertEquals(5, extension.maxOfThree(5, 4, 3));
+    public void testMaxOfThree_first() {
+        assertEquals(5, extension.maxOfThree(5, 2, 3));
     }
 
     @Test
-    void testMaxOfThree_third() {
-        assertEquals(5, extension.maxOfThree(3, 4, 5));
+    public void testMaxOfThree_third() {
+        assertEquals(10, extension.maxOfThree(3, 4, 10));
     }
 
     @Test
-    void testMedian_four() {
+    public void testMedian_four() {
         assertEquals(5, extension.median(Arrays.asList(7,5,3,5)));
     }
 
     @Test
-    void testMedian_five() {
+    public void testMedian_five() {
         assertEquals(3, extension.median(Arrays.asList(1,2,3,4,5)));
     }
 
     @Test
-    void testIsVowel_a() {
+    public void testIsVowel_a() {
         assertTrue(extension.isVowel('a'));
     }
 
     @Test
-    void testIsVowel_u() {
+    public void testIsVowel_u() {
         assertTrue(extension.isVowel('u'));
     }
 
     @Test
-    void testTranslate_bemutatkozik() {
+    public void testTranslate_bemutatkozik() {
         assertEquals("bevemuvutavatkovozivik", extension.translate("bemutatkozik"));
     }
 
     @Test
-    void testTranslate_lagopus() {
+    public void testTranslate_lagopus() {
         assertEquals("lavagovopuvus", extension.translate("lagopus"));
     }
 }
