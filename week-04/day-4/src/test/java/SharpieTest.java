c6e15d0763abdea1775ed893f09a12637857a160
Jan Sorna <honzasorna@gmail.com>
2019-04-25 15:24:46 +0200

    Finished Sharpie exercise

--- week-04/day-4/src/test/java/SharpieTest.java
+++ week-04/day-4/src/test/java/SharpieTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SharpieTest {
+
+    Sharpie mySharpie;
+    Sharpie mySharpie2;
+
+    @Before
+    public void methodBefore(){
+        mySharpie = new Sharpie("blue", 10f);
+        mySharpie = new Sharpie(null, null);
+    }
+
+    @Test
+    public void Sharpie_initializingNewSharpie_toBeInitialized(){
+        assertNotNull(mySharpie);
+    }
+
+    @Test
+    public void Sharpie_initializingNewNullSharpie_toNotBeInitialized(){
+        assertNull(mySharpie2);
+    }
+
+    @Test
+    public void Sharpie_initializedSharpieHasCorrectInkAmount_InkAmountEquals100(){
+        float correctInkAmount = 100f;
+        assertEquals( correctInkAmount, mySharpie.inkAmount, 0);
+    }
+
+    @Test
+    public void use_inputSharpie_InkAmountIsDecreasing(){
+        mySharpie.use();
+        assertEquals(99,mySharpie.inkAmount ,0);
+    }
 }
