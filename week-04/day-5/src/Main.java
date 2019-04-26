21415d3d3064082cc3cc37bbdac60a1e19dcaa8d
Jan Sorna <honzasorna@gmail.com>
2019-04-26 14:14:23 +0200

    Finished -r and handled all exceptions for it

--- week-04/day-5/src/Main.java
+++ week-04/day-5/src/Main.java
@@ -6,55 +6,76 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Path tasksPath = Paths.get("D:\\Desktop\\GFA\\greenfox\\jansorna\\week-04\\day-5\\src\\tasks.txt");
 
-        Path tasksPath = Paths.get("D:\\Desktop\\GFA\\greenfox\\jansorna\\week-04\\day-5\\src\\tasks.txt");
+            if (args.length == 0) {
+                System.out.println("Command Line Todo application");
+                System.out.println("=============================");
+                System.out.println();
+                System.out.println("Command line arguments:");
+                System.out.println("-l   Lists all the tasks");
+                System.out.println("-a   Adds a new task");
+                System.out.println("-r   Removes a task");
+                System.out.println("-c   Completes a task");
+            }
 
-        if (args.length == 0) {
-            System.out.println("Command Line Todo application");
-            System.out.println("=============================");
-            System.out.println();
-            System.out.println("Command line arguments:");
-            System.out.println("-l   Lists all the tasks");
-            System.out.println("-a   Adds a new task");
-            System.out.println("-r   Removes a task");
-            System.out.println("-c   Completes a task");
-        }
+            if (args[0].equals("-l")) {
+                try {
+                    List<String> lines = Files.readAllLines(tasksPath);
 
-        if (args[0].equals("-l")) {
+                    if (lines.isEmpty()) {
+                        System.out.println("No todos for today! :)");
+                    }
+
+                    for (int i = 0; i < lines.size(); i++) {
+                        System.out.println((i + 1) + " - " + lines.get(i));
+                    }
+                } catch (IOException e) {
+                    System.out.println("Something went wrong wit tasks file.");
+                }
+            }
+
+            if (args[0].equals("-a")) {
+                if (args.length == 1) {
+                    System.out.println("Unable to add: no task provided");
+                }
+                try {
+                    List<String> content = Files.readAllLines(tasksPath);
+
+                    for (int i = 0; i < args.length - 1; i++) {
+                        content.add(args[i + 1]);
+                    }
+                    Files.write(tasksPath, content);
+                } catch (IOException e) {
+                    System.out.println("Something went wrong wit tasks file.");
+                }
+            }
+
             try {
-                List<String> lines = Files.readAllLines(tasksPath);
+                if (args[0].equals("-r")) {
+                    if (args.length == 1) {
+                        System.out.println("Unable to remove: no index provided");
+                    }
+                    int numberOfRemoval = Integer.parseInt(args[1]);
 
-                if (lines.isEmpty()) {
-                    System.out.println("No todos for today! :)");
-                }
+                    try {
+                        List<String> content = Files.readAllLines(tasksPath);
+                        content.remove(numberOfRemoval + 1);
+                        Files.write(tasksPath, content);
 
-                for (int i = 0; i < lines.size(); i++) {
-                    System.out.println((i + 1) + " - " + lines.get(i));
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong wit tasks file.");
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println("Unable to remove: index is out of bound.");
+                    }
                 }
-            } catch (IOException e) {
-                System.out.println("Something went wrong wit tasks file.");
+            } catch (NumberFormatException e) {
+                System.out.println("Unsupported argument.");
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
         }
-
-        if (args[0].equals("-a")) {
-
-            if (args.length == 1) {
-                System.out.println("Unable to add: no task provided");
-            }
-
-            try {
-                List<String> content = Files.readAllLines(tasksPath);
-
-                for (int i = 0; i < args.length - 1; i++) {
-                    content.add(args[i + 1]);
-                }
-                Files.write(tasksPath, content);
-            } catch (IOException e) {
-                System.out.println("Something went wrong wit tasks file.");
-            }
-        }
-
-
-
     }
 }
+
+
