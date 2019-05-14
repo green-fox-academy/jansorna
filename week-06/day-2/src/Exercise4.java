e186c435d65c8785421788c055cd7510d71b3842
Jan Sorna <honzasorna@gmail.com>
2019-05-14 13:58:28 +0200

    Finished fourth exercise

---
+++ week-06/day-2/src/Exercise4.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        IntSummaryStatistics averageOfOddNumbers = numbers.stream()
+                .filter(a -> a % 2 != 0)
+                .mapToInt(Integer::intValue)
+                .summaryStatistics();
+        System.out.println(averageOfOddNumbers.getAverage());
+    }
+}
